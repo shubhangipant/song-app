c7791df3739cf15fa67939b7530a9dba59d4e397
Shubhangi <pantshubhangi96@gmail.com>
2017-08-14 01:20:29 +0530

    progress in voice recognition

--- main.js
+++ main.js
@@ -1,94 +1,91 @@
-var currentSongNumber = 1;
-var willLoop = 0;
-var willShuffle = 0; // will use this soon
-function timeJump() {
-    var song = document.querySelector('audio')
-    song.currentTime = song.duration - 5;
+var Playingnumber = 0  ;
+var shuffle=0;
+var equal = 0;
+var loop = 0;
+
+
+
+function fancyTimeFormat(time) {
+    // Hours, minutes and seconds
+    var hrs = ~~(time / 3600);
+    var mins = ~~((time % 3600) / 60);
+    var secs = time % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
 }
 
+// time function end
+
+function toggleSong() {
+    var song = document.querySelector('audio');
+    if (song.paused == true) {
+        console.log('Playing');
+        $('.play-icon').removeClass('fa-play').addClass('fa-pause');
+        song.play();
+    } else {
+        console.log('Pausing');
+        $('.play-icon').removeClass('fa-pause').addClass('fa-play');
+        song.pause();
+    }
+}
+
+function updateCurrentTime() {
+    var song = document.querySelector('audio');
+    var currentTime = Math.floor(song.currentTime);
+    currentTime = fancyTimeFormat(currentTime);
+    var duration = Math.floor(song.duration);
+    duration = fancyTimeFormat(duration)
+    $('.time-elapsed').text(currentTime);
+    $('.song-duration').text(duration);
+}
+
+
 $('.welcome-screen button').on('click', function() {
-     var name = $('#name-input').val();
-     if (name.length > 2) {
-         var message = "Welcome, " + name;
-         $('.main .user-name').text(message);
-         $('.welcome-screen').addClass('hidden');
-         $('.main').removeClass('hidden');
-     } else {
-         $('#name-input').addClass('error');
-     }
- });
-
-
- function fancyTimeFormat(time)
- {
- // Hours, minutes and seconds
- var hrs = ~~(time / 3600);
- var mins = ~~((time % 3600) / 60);
- var secs = time % 60;
-
- // Output like "1:01" or "4:03:59" or "123:03:59"
- var ret = "";
-
- if (hrs > 0) {
-     ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
- }
-
- ret += "" + mins + ":" + (secs < 10 ? "0" : "");
- ret += "" + secs;
- return ret;
- }
-
- function toggleSong()// what we have done is created a function toggle song jisme hum icon play and pause dekhenge
-  {
- var song = document.querySelector('audio');
- if(song.paused == true)//agar statement true h to
- {
- console.log('Playing');
- $('.play-icon').removeClass('fa-play').addClass('fa-pause');// play icon ko remove krke hum pause icon laga denge
- song.play();
- }
- else {
- console.log('Pausing');
- $('.play-icon').removeClass('fa-pause').addClass('fa-play');// agar ni h condition true to hum pause ko remove krenge aur play class ko add krenge
- song.pause();
- }
- }
- function updateCurrentTime() // yaha pe we have a made a function that will do the following steps
- {
- var song = document.querySelector('audio');//variable song me query selector se audio ko lena is been donw with this function that we made.
-// console.log(song.currentTime);//console.log humara currentTime lena hai
-// console.log(song.duration);// yaha pe console.log humara song k duration ko lera h
- var currentTime = Math.floor(song.currentTime);// variable currentTime se nikal ra h math.floor k andar song k current time ko
- currentTime = fancyTimeFormat(currentTime);//current time ko nikal k fancyTimeFormat waale function m pass krke naya time nikala h fir dubara current time m pass kia us processed tie ko
- var duration = Math.floor(song.duration);//variable duration se nikal ra h math.floor k andar song k duration ko
- duration = fancyTimeFormat(duration);
- $('.time-elapsed').text(currentTime);// text function humara jo humare current time k duration h use display kr dega
- $('.song-duration').text(duration);// ab text se hum duration ko pass kr re jo variablehumne upar bnaya tha
- }
-
-
-
- $('.play-icon').on('click', function() {
-    toggleSong();//calling of function that we created (toggleSong)
- });
- $('body').on('keypress',function(event) //body pe jb keypress kraenge to ye function chalega
-//event ek object hai humara so that we can use it
- {
-    var target = event.target;
-    if (event.keyCode == 32 && target.tagName !='INPUT')//event k andar target m tagname k andar we have a information k kause tag p lagaya h humne
-    {
+    var name = $('#name-input').val();
+    if (name.length > 2) {
+        var message = "Welcome, " + name;
+        $('.main .user-name').text(message);
+        $('.welcome-screen').addClass('hidden');
+        $('.main').removeClass('hidden');
+    } else {
+        $('#name-input').addClass('error');
+    }
+});
+$('.play-icon').on('click', function() {
+    toggleSong();
+});
+$('body').on('keypress', function(event) {
+    if (event.keyCode == 32 && event.target.tagName !='INPUT') {
         toggleSong();
+
     }
 });
 
+// var songList = ['Tamma Tamma',
+//     'Humma Song','Nashe si chad gayi','The Breakup Song'
+// ];
+//
+// var fileNames = ['song1.mp3', 'song2.mp3', 'song3.mp3', 'song4.mp3'];
+// var artistList = [' Neha Kakkar, Monali Thakur, Ikka Singh, Dev Negi','Badshah, Jubin Nautiyal, Shashaa Tirupati','Arijit Singh','Nakash Aziz, Arijit Singh, Badshah, Jonita Gandhi'];
+// var albumList = ['Badrinath ki Dulhania','Ok Jaanu','Befikre','Ae Dil Hai Mushkil'];
+// var durationList = ['2:56','3:15','2:34','2:29'];
 
-        var songs = [{
+var songs = [{
         'name': 'Badri Ki Dulhania (Title Track)',
         'artist': 'Neha Kakkar, Monali Thakur, Ikka Singh, Dev Negi',
         'album': 'Badrinath ki Dulhania',
         'duration': '2:56',
-       'fileName': 'song1.mp3',
-       'image':'QTdBSCq - Imgur.jpg'
+        'fileName': 'song1.mp3',
+        'image': 'download.jpg'
     },
     {
         'name': 'Humma Song',
@@ -96,7 +93,7 @@ $('.welcome-screen button').on('click', function() {
         'album': 'Ok Jaanu',
         'duration': '3:15',
         'fileName': 'song2.mp3',
-        'image':'download.jpg'
+        'image': 'download (3).jpg'
     },
     {
         'name': 'Nashe Si Chadh Gayi',
@@ -104,7 +101,7 @@ $('.welcome-screen button').on('click', function() {
         'album': 'Befikre',
         'duration': '2:34',
         'fileName': 'song3.mp3',
-        'image':'download (1).jpg'
+        'image': 'download (1).jpg'
     },
     {
         'name': 'The Breakup Song',
@@ -112,92 +109,418 @@ $('.welcome-screen button').on('click', function() {
         'album': 'Ae Dil Hai Mushkil',
         'duration': '2:29',
         'fileName': 'song4.mp3',
-        'image':'download (2).jpg'
-    }]
-    window.onload = function() {
+        'image': 'download (2).jpg'
+    }
+]
 
 
-      function changeCurrentSongDetails(songObj) {
-          $('.current-song-image').attr('src','img/' + songObj.image)
-          $('.current-song-name').text(songObj.name)
-          $('.current-song-album').text(songObj.album)
-      }
+//
 
- function addSongNameClickEvent(songObj,position) //is function me humara jo user h wo input dera h
- {
- var songName=songObj.fileName;
- var id = '#song' + position;// isme agar position 1 hai to song id b 1 hogi and so on....
- $(id).click(function() {
- var audio = document.querySelector('audio');
- var currentSong = audio.src;
- if(currentSong.search(songName) != -1)//agar song same h to toggle song krna h
- {
- toggleSong();
- }
- else {
- audio.src = songName;//warna song ka source b change krna h toggle song b karna h aur song details b change kri h
- toggleSong();
- changeCurrentSongDetails(songObj);
- }
- });
- }
-changeCurrentSongDetails(songs[0]);
+function updateTimer(){
+var song = document.querySelector('audio');
+var ct =song.currentTime;
+var td =song.duration;
+var percentage = (ct/td)*100;
+$(".progress-filled").css('width',percentage+"%");
 
- for(var i =0; i < songs.length;i++) {
-   var obj = songs[i];
-   var name = '#song' + (i+1);
-   var song = $(name);
-   song.find('.song-name').text(obj.name);
-   song.find('.song-artist').text(obj.artist);
-   song.find('.song-album').text(obj.album);
-   song.find('.song-length').text(obj.duration);
-   addSongNameClickEvent(obj,i+1)
- }
-$('#songs').DataTable({
+
+
+}
+
+
+$(".player-progress").click(function(event) {
+    var $this = $(this);
+
+    // to get part of width of progress bar clicked
+    var widthclicked = event.pageX - $this.offset().left;
+    var totalWidth = $this.width(); // can also be cached somewhere in the app if it doesn't change
+
+    // do calculation of the seconds clicked
+    var calc = (widthclicked / totalWidth) * 100 ; // get the percent of bar clicked and multiply in by the duration
+
+
+var song = document.querySelector('audio');
+song.currentTime = (song.duration*calc)/100;
+
+updateTimer();
+
+
+
+});
+
+
+
+
+
+
+
+///////////
+
+
+
+
+
+function changeCurrentSongDetails(songObj) //function creat kiya ek argument pass kiya hai song object
+{
+    $('.current-song-image').attr('src', 'img/' + songObj.image);
+    $('.current-song-name').text(songObj.name);
+    $('.current-song-album').text(songObj.album);
+}
+
+function addSongNameClickEvent(songObj, position) //we have made a machine jispe 2 buttons diye hai songName and position ke liye
+{
+
+    var songName = songObj.fileName;
+    var id = '#song' + position; //#song ke saath position ko jod do and agar position 1 hai to output #song1 hogi jisse id mein store kar diya
+    $(id).click(function() //agar #song1 hai to one vaale div pe event lage ga
+        {
+              Playingnumber= (position - 1)
+            var audio = document.querySelector('audio');
+            var currentSong = audio.src;
+            if (currentSong.search(songName) != -1) {
+                toggleSong();
+            } else {
+                audio.src = songName;
+                toggleSong();
+                changeCurrentSongDetails(songObj);
+            }
+        });
+}
+
+
+function changeSong() //we have made a machine jispe 2 buttons diye hai songName and position ke liye
+{
+var music =  songs[Playingnumber].fileName;
+var song = document.querySelector("audio");
+song.src = music;
+toggleSong();
+changeCurrentSongDetails(songs[Playingnumber])
+}
+window.onload = function() {
+
+
+
+    changeCurrentSongDetails(songs[0]);
+
+    for (var i = 0; i < songs.length; i++) //Var i zero se 3 tak chalana hai
+
+    {
+        var obj = songs[i]; //Diary ke andar se page utha ke humne obj variable mein store kar diya
+        var name = '#song' + (i + 1);
+        var song = $(name);
+        song.find('.song-name').text(obj.name); //("song1 .songname")
+        song.find('.song-artist').text(obj.artist);
+        song.find('.song-album').text(obj.album);
+        song.find('.song-length').text(obj.duration);
+        addSongNameClickEvent(obj, i + 1);
+    }
+    updateCurrentTime();
+    setInterval(function() {
+        updateCurrentTime();
+    }, 1000);
+
+    setInterval(function() {
+        updateTimer();
+    }, 1000);
+
+
+
+    $("#songs").DataTable({
         paging: false
     });
-    $('.fa-repeat').on('click',function() {
-        $('.fa-repeat').toggleClass('disabled')
-        willLoop = 1 - willLoop;
-    });
+}
 
-    $('.fa-random').on('click',function() {
-        $('.fa-random').toggleClass('disabled')
-        willShuffle = 1 - willShuffle;
-    });
-    $('audio').on('ended',function() {
-        var audio = document.querySelector('audio');
-        if (willShuffle == 1) {
-            var nextSongNumber = randomExcluded(1,4,currentSongNumber); // Calling our function from Stackoverflow
-            var nextSongObj = songs[nextSongNumber-1];
-            audio.src = nextSongObj.fileName;
-            toggleSong();
-            changeCurrentSongDetails(nextSongObj);
-            currentSongNumber = nextSongNumber;
-        }
-        else if(currentSongNumber < 4) {
-            var nextSongObj = songs[currentSongNumber];
-            audio.src = nextSongObj.fileName;
-            toggleSong();
-            changeCurrentSongDetails(nextSongObj);
-            currentSongNumber = currentSongNumber + 1;
-        }
-        else if(willLoop == 1) {
-            var nextSongObj = songs[0];
-            audio.src = nextSongObj.fileName;
-            toggleSong();
-            changeCurrentSongDetails(nextSongObj);
-            currentSongNumber =  1;
-        }
-        else {
-            $('.play-icon').removeClass('fa-pause').addClass('fa-play');
-            audio.currentTime = 0;
-        }
-    })
+function randomExcluded(min, max, excluded) {
+    var n = Math.floor(Math.random() * (max-min) + min);
+    if (n >= excluded) n++;
+    return n;
+}
 
-       updateCurrentTime();// yaha pe humara upar waala function call hua k jaise he hum apna page refresh kare uske sath2 he humara time show hojae
-       setInterval(function()// yaha pe humne ek aur function bnaya jo updateCurrentTime ko call kr ra h
-        {
-       updateCurrentTime();
-     },1000);// ye jo 1000 value humne di h this means 1 sec aur iski wajah se humara page har ek second m update hora h.
-            }
+
+$(".fa-step-forward").click(function(){
+
+if(shuffle==1)
+{
+      var audio = document.querySelector('audio');
+      var nextSongNumber = randomExcluded(0,3,Playingnumber); // Calling our function from Stackoverflow
+
+      var nextSongObj = songs[nextSongNumber];
+      audio.src = nextSongObj.fileName;
+      toggleSong();
+      changeCurrentSongDetails(nextSongObj);
+      Playingnumber = nextSongNumber;
+
+
+}
+
+
+else {
+
+          if(Playingnumber == songs.length-1){
+          Playingnumber = 0;
+          changeSong();
+          }
+
+          else {
+          console.log("two");
+          console.log(Playingnumber);
+            Playingnumber++;
+          changeSong();
+          }
+
+}
+
+})
+
+
+
+
+$(".fa-step-backward").click(function(){
+
+if(Playingnumber == 0){
+console.log("one");
+Playingnumber = (songs.length-1);
+changeSong();
+
+
+
+
+}
+
+else {
+console.log("two");
+console.log(Playingnumber);
+  Playingnumber--;
+changeSong();
+}
+
+
+
+
+})
+
+// function shufflee(a) {
+//     var j, x, i;
+//     for (i = a.length; i; i--) {
+//         j = Math.floor(Math.random() * i);
+//         x = a[i - 1];
+//         a[i - 1] = a[j];
+//         a[j] = x;
+//     }
+// }
+
+
+
+$(".fa-random").click(function(){
+
+
+$(".fa-random").toggleClass("active");
+if(shuffle==0){
+
+shuffle=1;
+}
+else {
+  shuffle=0;
+}
+// if(shuffle==0)
+// {
+// shuffle = 1;
+// shufflee(songs);
+// changeCurrentSongDetails(songs[0]);
+// song =document.querySelector("audio");
+// song.src = songs[0].fileName;
+// toggleSong();
+// Playingnumber=0;
+//
+//     for (var i = 0; i < songs.length; i++) //Var i zero se 3 tak chalana hai
+//
+//     {
+//         var obj = songs[i]; //Diary ke andar se page utha ke humne obj variable mein store kar diya
+//         var name = '#song' + (i + 1);
+//         var song = $(name);
+//         song.find('.song-name').text(obj.name); //("song1 .songname")
+//         song.find('.song-artist').text(obj.artist);
+//         song.find('.song-album').text(obj.album);
+//         song.find('.song-length').text(obj.duration);
+//         addSongNameClickEvent(obj, i + 1);
+//     }
+// }
+
+// else {
+//   shuffle = 0;
+//
+//    songs = [{
+//           'name': 'Badri Ki Dulhania (Title Track)',
+//           'artist': 'Neha Kakkar, Monali Thakur, Ikka Singh, Dev Negi',
+//           'album': 'Badrinath ki Dulhania',
+//           'duration': '2:56',
+//           'fileName': 'song1.mp3',
+//           'image': 'song1.jpg'
+//       },
+//       {
+//           'name': 'Humma Song',
+//           'artist': 'Badshah, Jubin Nautiyal, Shashaa Tirupati',
+//           'album': 'Ok Jaanu',
+//           'duration': '3:15',
+//           'fileName': 'song2.mp3',
+//           'image': 'song2.jpg'
+//       },
+//       {
+//           'name': 'Nashe Si Chadh Gayi',
+//           'artist': 'Arijit Singh',
+//           'album': 'Befikre',
+//           'duration': '2:34',
+//           'fileName': 'song3.mp3',
+//           'image': 'song3.jpg'
+//       },
+//       {
+//           'name': 'The Breakup Song',
+//           'artist': 'Nakash Aziz, Arijit Singh, Badshah, Jonita Gandhi',
+//           'album': 'Ae Dil Hai Mushkil',
+//           'duration': '2:29',
+//           'fileName': 'song4.mp3',
+//           'image': 'song4.jpg'
+//       }
+//   ]
+//
+//   changeCurrentSongDetails(songs[0]);
+//   song =document.querySelector("audio");
+//   song.src = songs[0].fileName;
+// toggleSong();
+// Playingnumber=0;
+//   for (var i = 0; i < songs.length; i++) //Var i zero se 3 tak chalana hai
+//
+//   {
+//       var obj = songs[i]; //Diary ke andar se page utha ke humne obj variable mein store kar diya
+//       var name = '#song' + (i + 1);
+//       var song = $(name);
+//       song.find('.song-name').text(obj.name); //("song1 .songname")
+//       song.find('.song-artist').text(obj.artist);
+//       song.find('.song-album').text(obj.album);
+//       song.find('.song-length').text(obj.duration);
+//       addSongNameClickEvent(obj, i + 1);
+//   }
+//
+//
+// }
+//
+
+
+
+
+
+})
+
+
+$(".fa-bar-chart").click(function(){
+
+$(this).toggleClass("active");
+if(equal==0)
+{
+
+equal=1;
+
+$("svg").css("display","inline-block");
+$(".content").css("display","none");
+$(".contain").css("display","inline-block");
+$(".contain").css("background","black");
+
+
+}
+else{
+equal=0;
+$("svg").css("display","none");
+$(".content").css("display","inline-block");
+$(".contain").css("display","none");
+
+
+
+
+}
+
+
+
+
+
+
+
+})
+
+$(".fa-repeat").click(function(){
+$(".fa-repeat").toggleClass("active");
+if(loop==0){
+
+loop=1;
+
+}
+else{
+
+loop=0;
+
+}
+
+
+})
+
+function randomExcluded(min, max, excluded) {
+    var n = Math.floor(Math.random() * (max-min) + min);
+    if (n >= excluded) n++;
+    return n;
+}
+
+
+
+$('audio').on('ended',function() {
+    var audio = document.querySelector('audio');
+    if (shuffle == 1) {
+            var nextSongNumber = randomExcluded(0,3,Playingnumber); // Calling our function from Stackoverflow
+console.log(nextSongNumber);
+            console.log(nextSongNumber);
+            var nextSongObj = songs[nextSongNumber];
+            audio.src = nextSongObj.fileName;
+            toggleSong();
+            changeCurrentSongDetails(nextSongObj);
+            Playingnumber = nextSongNumber;
+
+        }
+    else if(Playingnumber < 3) {
+  console.log("hello2");
+        var nextSongObj = songs[Playingnumber+1];
+        audio.src = nextSongObj.fileName; // Change Soure
+        toggleSong(); // Play Next Song
+        changeCurrentSongDetails(nextSongObj); // Update Image
+        Playingnumber ++;// Change State
+    }
+    else if(loop == 1) {
+  console.log("hello3");
+         var nextSongObj = songs[0];
+         audio.src = nextSongObj.fileName;
+         toggleSong();
+         changeCurrentSongDetails(nextSongObj);
+         Playingnumber =  1;
+     }
+     else {
+         $('.play-icon').removeClass('fa-pause').addClass('fa-play');
+         audio.currentTime = 0;
+     }
+})
+
+
+//   addSongNameClickEvent(fileNames[0],1);
+// addSongNameClickEvent(fileNames[1],2);
+// addSongNameClickEvent(fileNames[2],3);
+// addSongNameClickEvent(fileNames[3],4);
+
+$(".fa-microphone").hover(function(){
+
+$("ol").css("display","inline-block")
+
+
+})
+
+$(".fa-microphone").mouseout(function(){
+
+$("ol").css("display","none")
+
+
+})
